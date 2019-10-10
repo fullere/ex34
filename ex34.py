4e1915e7ef0b460cdbbf6d5ffbb091bac1b4fa7e
fullere <fullere@hartwick.edu>
2019-10-10 07:20:25 -0400

    added code

--- ex34.py
+++ ex34.py
@@ -1,2 +1,30 @@
 # Elizabeth Fuller
 # 10/9/19
+#
+
+animals = ['bear', 'python3.6', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+
+print(f"The 2nd animal is at 1 and is a {animals[1]}.")
+print(f"The animal at 1 is the 2nd animal and is a {animals[1]}.")
+
+print(f"The 3rd animal is at 4 and is a {animals[2]}.")
+print(f"The animal at 4 is the 3rd animal and is a {animals[2]}.")
+
+print(f"The 1st animal is at 0 and is a {animals[0]}.")
+print(f"The animal at 0 is the 1st animal and is a {animals[0]}.")
+
+print(f"The 4th animal is at 3 and is a {animals[3]}.")
+print(f"The animal at 3 is the 4th animal and is a {animals[3]}.")
+
+print(f"The 5th animal is at 4 and is a {animals[4]}.")
+print(f"The animal at 4 is the 5th animal and is a {animals[4]}.")
+
+print(f"The 3rd animal is at 2 and is a {animals[2]}.")
+print(f"The animal at 2 is the 3rd animal and is a {animals[2]}.")
+
+print(f"The 6th animal is at 5 and is a {animals[5]}.")
+print(f"The animal at 5 is the 6th animal and is a {animals[5]}.")
+
+print(f"The 5th animal is at 4 and is a {animals[4]}.")
+print(f"The animal at 4 is the 5th animal and is a {animals[4]}.")
